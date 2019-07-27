774478904b9a92dfb9d1fc396a81127f843a2aca
Olawale Onabola <walexi4great@gmail.com>
2019-07-27 11:33:06 +0100

    editing dataloader

--- src/dataloader.py
+++ src/dataloader.py
@@ -166,7 +166,7 @@ class Loader(data.Dataset):
         # convert to tensor (num_tasks x (num_ways * (num_supports + num_queries)) x ...)
         support_data = torch.stack([data.to(tt.arg.device) for data in support_data], 1)
         support_label = torch.stack([torch.tensor(label).float().to(tt.arg.device) for label in support_label], 0)
-        query_data = torch.stack([data.to(tt.arg.device) for data in query_data], 1)
+        query_data = torch.stack([torch.from_numpy(data).float().to(tt.arg.device) for data in query_data], 1)
         query_label = torch.stack([torch.tensor(label).float().to(tt.arg.device) for label in query_label], 0)
 
         return [support_data, support_label, query_data, query_label]
