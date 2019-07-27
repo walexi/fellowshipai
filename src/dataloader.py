5129682117d2ef11f2232cf98b93b6c9a90fbb8b
Olawale Onabola <walexi4great@gmail.com>
2019-07-27 13:04:18 +0100

    editing learner

--- src/dataloader.py
+++ src/dataloader.py
@@ -159,10 +159,15 @@ class Loader(data.Dataset):
                     query_data[i_idx + c_idx * num_queries][t_idx] = self.transform(class_data_list[num_shots + i_idx])
                     query_label[i_idx + c_idx * num_queries][t_idx] = c_idx
 
-        # convert to tensor (num_tasks x (num_ways * (num_supports + num_queries)) x ...)
+        # convert to tensor (num_tasks x (num_ways * (num_supports + num_queries)) x ...) 
+        #support_data = (num_task, 1, 84, 84) label= (num_tasks)
+        # num_tasks * data_size * num_ways * num_shots
+        #
         support_data = torch.stack([torch.from_numpy(data).float().to(tt.arg.device) for data in support_data], 1)
         support_label = torch.stack([torch.from_numpy(label).float().to(tt.arg.device) for label in support_label], 1)
         query_data = torch.stack([torch.from_numpy(data).float().to(tt.arg.device) for data in query_data], 1)
         query_label = torch.stack([torch.from_numpy(label).float().to(tt.arg.device) for label in query_label], 1)
 
+        print(support_data.shape)
+        print(query_data.shape)
         return [support_data, support_label, query_data, query_label]
